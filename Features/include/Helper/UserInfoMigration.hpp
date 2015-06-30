36766b8efe988c0ed3f1eb8218dabee80644452c
Ilya Tkachenko <Ilyatk@users.noreply.github.com>
2015-06-30 14:40:36 +0300

    Remove the setting deletion after migration

--- Features/include/Helper/UserInfoMigration.hpp
+++ Features/include/Helper/UserInfoMigration.hpp
@@ -32,17 +32,17 @@ void migrateUserInfo()
     return;
 
   QString appRoot = QCoreApplication::applicationDirPath();
-  FileUtils::removeRecursively(QString("%1/Cache").arg(appRoot));
+  //FileUtils::removeRecursively(QString("%1/Cache").arg(appRoot));
 
   QString newTorrentsPath = QStandardPaths::writableLocation(QStandardPaths::DataLocation) + "/torrents";
   QString oldTorrentsPath = QString("%1/torrents").arg(appRoot);
   FileUtils::recurseCopyDiretory(oldTorrentsPath, newTorrentsPath);
-  FileUtils::removeRecursively(oldTorrentsPath);
+  //FileUtils::removeRecursively(oldTorrentsPath);
 
   QString newMessengerPath = QStandardPaths::writableLocation(QStandardPaths::DataLocation) + "/db/Account";
   QString oldMessengerPath = QString("%1/Account").arg(appRoot);
   FileUtils::recurseCopyDiretory(oldMessengerPath, newMessengerPath);
-  FileUtils::removeRecursively(oldMessengerPath);
+  //FileUtils::removeRecursively(oldMessengerPath);
 
   QString newSettingsPath = QStandardPaths::writableLocation(QStandardPaths::DataLocation) + "/settings";
   FileUtils::createDirectory(newSettingsPath);
@@ -50,8 +50,8 @@ void migrateUserInfo()
   QString hostSettingsPath = QString("%1/host.sql").arg(appRoot);
   QFile::copy(settingsPath, QString("%1/settings.sql").arg(newSettingsPath));
   QFile::copy(hostSettingsPath, QString("%1/host.sql").arg(newSettingsPath));
-  QFile::remove(settingsPath);
-  QFile::remove(hostSettingsPath);
+  //QFile::remove(settingsPath);
+  //QFile::remove(hostSettingsPath);
 
   QString newLogsPath = QStandardPaths::writableLocation(QStandardPaths::DataLocation) + "/logs";
   FileUtils::createDirectory(newLogsPath);
@@ -65,13 +65,13 @@ void migrateUserInfo()
   QFile::copy(qgnaLog, QString("%1/qgna.log").arg(newLogsPath));
   QFile::copy(qgnaLog1, QString("%1/qgna.log.1").arg(newLogsPath));
 
-  QFile::remove(hostLog);
-  QFile::remove(hostLog1);
-  QFile::remove(qgnaLog);
-  QFile::remove(qgnaLog1);
+  //QFile::remove(hostLog);
+  //QFile::remove(hostLog1);
+  //QFile::remove(qgnaLog);
+  //QFile::remove(qgnaLog1);
 
   QString integrationPath = QString("%1/Integration/ZZima/ZZimaLauncher.exe").arg(QCoreApplication::applicationDirPath());
-  FileUtils::removeRecursively(integrationPath);
+  //FileUtils::removeRecursively(integrationPath);
 
   checkUserInfoMigration(true);
 }
