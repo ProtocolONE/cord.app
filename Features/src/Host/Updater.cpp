10a93d29d41a075a865ca9e56583f10ecb8a7866
Ilya Tkachenko <Ilyatk@users.noreply.github.com>
2016-05-20 09:38:16 +0300

    Change updating domain to fs0.gnfiles.com

--- Features/src/Host/Updater.cpp
+++ Features/src/Host/Updater.cpp
@@ -127,7 +127,7 @@ namespace GameNet {
       installUpdateGnaPath = QString("tst");
 #endif
 
-      QString updateUrl = QString("https://fs0.gamenet.ru/update/qgna/%1/").arg(updateArea);
+      QString updateUrl = QString("https://fs0.gnfiles.com/update/qgna/%1/").arg(updateArea);
       QString updateCrc = QString("%1update.crc.7z").arg(updateUrl);
       this->_checkUpdateHelper.setUpdateUrl(updateCrc);
 
