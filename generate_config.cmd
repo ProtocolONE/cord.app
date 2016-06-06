157980214f9931e3d25ecab3c1acc9f6a9d76d5d
Kirill Vostrecov <kirillv@users.noreply.github.com>
2016-06-06 14:17:25 +0300

    Add build config batch

---
+++ generate_config.cmd
@@ -0,0 +1,78 @@
+Set BuildFolder=%~1
+
+echo %BuildFolder%
+
+Set OutConfig="%BuildFolder%\Thetta\description.dat"
+Set Hasher="%QGNACOMMONDIR%\GGS\SectionHasher\trunk\bin\SectionHasher.exe"
+
+:: qGna
+Set qgnapath="%BuildFolder%\qGNA.exe"
+Set qgnapathout="%BuildFolder%\qgna.json"
+
+:: GameDownloaderx86d.dll
+
+Set gamedownloader="%BuildFolder%GameDownloaderx86.dll"
+Set gamedownloaderout="%BuildFolder%\gamedownloader.json"
+
+:: GameExecutorX86d.dll
+
+Set gameexecutor="%BuildFolder%\GameExecutorX86.dll"
+Set gameexecutorout="%BuildFolder%\gameexecutor.json"
+
+:: CoreX86d.dll
+
+Set corex="%BuildFolder%\CoreX86.dll"
+Set corexout="%BuildFolder%\core.json"
+
+:: UpdateSystemX86d.dll
+
+Set updatesystem="%BuildFolder%\UpdateSystemX86.dll"
+Set updatesystemout="%BuildFolder%\updatesystem.json"
+
+:: Qt5Networkd.dll
+
+Set qtnetwork="%BuildFolder%\Qt5Network.dll"
+Set qtnetworkout="%BuildFolder%\qt5network.json"
+
+:: libeay32.dll
+
+Set libeay="%BuildFolder%\libeay32.dll"
+Set libeayout="%BuildFolder%\libeay32.json"
+
+:: ssleay32.dll
+
+Set ssleay="%BuildFolder%\ssleay32.dll"
+Set ssleayout="%BuildFolder%\ssleay32.json"
+
+:: Generating jsons
+
+call %Hasher% -c=%qgnapath% -o=%qgnapathout%
+call %Hasher% -c=%gamedownloader% -o=%gamedownloaderout%
+call %Hasher% -c=%gameexecutor% -o=%gameexecutorout%
+call %Hasher% -c=%corex% -o=%corexout%
+call %Hasher% -c=%updatesystem% -o=%updatesystemout%
+
+:: call SectionHasher.exe -c=Qt5Cored.dll -o=qt5core.json
+
+call %Hasher% -c=%qtnetwork% -o=%qtnetworkout%
+
+call %Hasher% -c=%libeay% -o=%libeayout%
+call %Hasher% -c=%ssleay% -o=%ssleayout%
+
+:: Merge all configs
+:: With qtcore
+:: call SectionHasher.exe -m=qgna.json,gamedownloader.json,gameexecutor.json,core.json,qt5core.json,updatesystem.json,qt5network.json,libeay32.json,ssleay32.json -o=description.dat
+call %Hasher% -m=%qgnapathout%,%gamedownloaderout%,%gameexecutorout%,%corexout%,%updatesystemout%,%qtnetworkout%,%libeayout%,%ssleayout% -o=%OutConfig%
+
+:: Delete all files
+
+del %qgnapathout%
+del %gamedownloaderout%
+del %gameexecutorout%
+del %corexout%
+del %updatesystemout%
+del %qtnetworkout%
+del %libeayout%
+del %ssleayout%
+
+
